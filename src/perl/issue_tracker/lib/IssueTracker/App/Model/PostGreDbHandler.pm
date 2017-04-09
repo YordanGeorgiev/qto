4c622b87747cbd20763cf055e4b8cc22be51ba9e
YordanGeorgiev <yordan.georgiev@gmail.com>
2017-04-09 17:54:54 +0300

    generate sql insert string visually ok

--- src/perl/issue_tracker/lib/IssueTracker/App/Model/PostGreDbHandler.pm
+++ src/perl/issue_tracker/lib/IssueTracker/App/Model/PostGreDbHandler.pm
@@ -7,27 +7,29 @@ package IssueTracker::App::Model::PostGreDbHandler ;
 	use Encode qw( encode_utf8 is_utf8 );
    use POSIX qw(strftime);
    use DBI ; 
-	use DBD::mysql;
 	use Data::Printer ; 
 	use Carp ; 
 
    use IssueTracker::App::Utils::Logger ; 
 
-   our $ModuleDebug	                                    = 0 ; 
-   our $IsUnitTest                                    	= 0 ; 
-	our $appConfig 													= q{} ; 
-	our $objLogger 													= q{} ; 
-	our $objController 												= {} ; 
+   our $module_trace                            = 1 ;
+   our $IsUnitTest                              = 0 ; 
+	our $appConfig 										= q{} ; 
+	our $objLogger 										= q{} ; 
+	our $objController 									= {} ; 
 
-	our $db_host 														= q{} ; 
-	our $db_port 														= q{} ;
-	our $db_user 														= q{} ; 
-	our $db_user_pw	 												= q{} ; 
-	our $web_host 														= q{} ; 
+	our $db_host 										   = q{} ; 
+	our $db_port 										   = q{} ;
+	our $db_user 											= q{} ; 
+	our $db_user_pw	 									= q{} ; 
+	our $web_host 											= q{} ; 
+
+
 
 	#
    # ------------------------------------------------------
-	# 
+	#  input: hash ref of hash refs containing the issues 
+	#  file data  and meta data 
    # ------------------------------------------------------
 	sub doInsertSqlHashData {
 
@@ -36,18 +38,37 @@ package IssueTracker::App::Model::PostGreDbHandler ;
       
       my $ret              = 1 ; 
       my $msg              = 'unknown error while sql insert ' ; 		
+      my $str_sql_insert   = q{} ; 
+      my $str_col_list     = q{} ; 
+      my $str_val_list     = q{} ; 
+
 
       my $debug_msg        = 'START doInsertSqlHashData' ; 
       $objLogger->doLogDebugMsg ( $debug_msg ) ; 
 	   
 		foreach my $key ( sort(keys( %{$sql_hash} ) ) ) {
+         my $row_hash = $sql_hash->{ $key } ; 
 
+		   foreach my $key ( sort(keys( %{$row_hash} ) ) ) {
+            $str_col_list .= ' , ' . $key ; 
+            $str_val_list .= ' , \'' . $row_hash->{ $key } . '\''; 
+         }
+         
+         $str_col_list = substr ( $str_col_list , 3 ) ; 
+         $str_val_list = substr ( $str_val_list , 3 ) ; 
+
+         $str_sql_insert .= 'INSERT INTO issue ' ; 
+         $str_sql_insert .= '( ' . $str_col_list . ') VALUES (' . $str_val_list . ');' . "\n" ; 
+
+         $str_col_list = '' ; 
+         $str_val_list = '' ; 
       }
-      #eof foreach $key
-
+      
 
       $debug_msg        = 'STOP  doInsertSqlHashData' ; 
       $objLogger->doLogDebugMsg ( $debug_msg ) ; 
+
+      p ( $str_sql_insert ) if $module_trace == 1 ; 
 
       return ( $ret , $msg ) ; 	
 	}
