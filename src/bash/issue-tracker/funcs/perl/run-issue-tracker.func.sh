19f9d80cb6f6f0bdace6e44ffba5aa76614d45ca
YordanGeorgiev <yordan.georgiev@gmail.com>
2017-04-11 22:39:02 +0300

    version 0.0.6

--- src/bash/issue-tracker/funcs/perl/run-issue-tracker.func.sh
+++ src/bash/issue-tracker/funcs/perl/run-issue-tracker.func.sh
@@ -13,18 +13,18 @@ doRunIssueTracker(){
 	# add your action implementation code here ... 
    nice_date=$(date "+%Y-%m-%d")
    nice_year=$(date "+%Y-%m")
-   issue_file="/vagrant/futu/futu-care/futu-care.1.0.1.prd.ysg/docs/txt/daily/""$nice_year""/""$nice_date"".daily-issues.ygeo.txt"
-   # issue_file='/vagrant/ysg/data/scrum/daily/'"$nice_year"'/'"$nice_date"'.daily-issues.ysg.txt'
-   # issue_file='/vagrant/futu/telia/doc/txt/'"$nice_year"'/'"$nice_date"'.futu-telia-issues.txt'
+   issues_file="/vagrant/futu/futu-care/futu-care.1.0.1.prd.ysg/docs/txt/daily/""$nice_year""/""$nice_date"".daily-issues.ygeo.txt"
+   # issues_file='/vagrant/ysg/data/scrum/daily/'"$nice_year"'/'"$nice_date"'.daily-issues.ysg.txt'
+   # issues_file='/vagrant/futu/telia/doc/txt/'"$nice_year"'/'"$nice_date"'.futu-telia-issues.txt'
 	
    # Action ... !!!
-   perl src/perl/issue_tracker/script/issue_tracker.pl --do file-to-db --issue_file $issue_file
+   perl src/perl/issue_tracker/script/issue_tracker.pl --do file-to-db --issues_file $issues_file
    exit_code=$?
    doLog "INFO doRunIssueTracker exit_code $exit_code"
    test $exit_code -ne 0 && doExit $exit_code "failed to run issue_tracker.pl"  
    
    # Action ... !!!
-	perl src/perl/issue_tracker/script/issue_tracker.pl --do db-to-xls
+	perl src/perl/issue_tracker/script/issue_tracker.pl --do db-to-xls --issues_file $issues_file
    exit_code=$?
    doLog "INFO doRunIssueTracker exit_code $exit_code"
    test $exit_code -ne 0 && doExit $exit_code "failed to run issue_tracker.pl"  
