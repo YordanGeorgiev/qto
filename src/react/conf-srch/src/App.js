4a62db9814ce5a07cf23971209cf4fb37b948af7
Yordan Georgiev <yordan.georgiev@gmail.com>
2017-07-29 19:59:50 +0300

    add auto-suggest

--- src/react/conf-srch/src/App.js
+++ src/react/conf-srch/src/App.js
@@ -1,21 +1,178 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Autosuggest from 'react-autosuggest';
 
-class App extends Component {
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'C#',
+    year: 2000
+  },
+  {
+    name: 'C++',
+    year: 1983
+  },
+  {
+    name: 'Clojure',
+    year: 2007
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  {
+    name: 'Go',
+    year: 2009
+  },
+  {
+    name: 'Haskell',
+    year: 1990
+  },
+  {
+    name: 'Java',
+    year: 1995
+  },
+  {
+    name: 'Javascript',
+    year: 1995
+  },
+  {
+    name: 'Perl',
+    year: 1987
+  },
+  {
+    name: 'PHP',
+    year: 1995
+  },
+  {
+    name: 'Python',
+    year: 1991
+  },
+  {
+    name: 'Ruby',
+    year: 1995
+  },
+  {
+    name: 'Scala',
+    year: 2003
+  }
+];
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+// https://github.com/moroshko/react-autosuggest/issues/27
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+class MyAutosuggest extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };    
+  }
+
+  onChange = (_, { newValue }) => {
+    const { id, onChange } = this.props;
+    
+    this.setState({
+      value: newValue
+    });
+    
+    onChange(id, newValue);
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
   render() {
+    const { id, placeholder } = this.props;
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder,
+      value,
+      onChange: this.onChange
+    };
+    
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Autosuggest 
+        id={id}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} 
+      />
     );
   }
 }
 
-export default App;
+class App extends Component {
+  onChange(id, newValue) {
+    console.log(`${id} changed to ${newValue}`);
+  }
+  
+  render() {
+    return (
+
+      <div className="App">
+        <div className="App-header">
+          <h2> srch for cnfs</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      <div>
+        <MyAutosuggest
+          id="type-c"
+          placeholder="Type 'c'"
+          onChange={this.onChange}
+        />
+      </div>
+
+      </div>
+
+    );
+  }
+}
+
+export default App ; 
+
