0f8c9445bb5ff2e81286d868a1b2df43af1b4f64
Yordan Georgiev <yordan.georgiev@gmail.com>
2017-10-04 20:05:59 +0300

    imroved error handling for run-pgsql-scripts

--- src/bash/issue-tracker/funcs/run-pgsql-scripts.func.sh
+++ src/bash/issue-tracker/funcs/run-pgsql-scripts.func.sh
@@ -1,8 +1,8 @@
 # file: src/bash/issue-tracker/funcs/run-pgsql-scripts.func.sh
 
-# v1.0.2
+# v1.0.3
 # ---------------------------------------------------------
-# the docs	
+# the docs	:
 # cat doc/txt/issue-tracker/funcs/run-pgsql-scripts.func.txt
 # ---------------------------------------------------------
 doRunPgsqlScripts(){
@@ -28,34 +28,39 @@ doRunPgsqlScripts(){
    psql -v ON_ERROR_STOP=1 -q -t -X -U "${pgsql_user:-}" \
        -v db_name="$db_name" -f "$sql_script" postgres > "$tmp_log_file" 2>&1
    ret=$?
+   doLog "INFO ret: $ret" 
    
    # show the user what is happenning 
    cat "$tmp_log_file"
 
-   test $ret -ne 0 && doExit $ret "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
+   test $ret -ne 0 && sleep 3
+   test $ret -ne 0 && export exit_code=1
+   test $ret -ne 0 && exit 1
+   test $ret -ne 0 && doExit "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
+   test $ret -ne 0 && break
  
 	# show the developer what happened
 	cat "$tmp_log_file" 
 
 	# and save the tmp log file into the log file
 	cat "$tmp_log_file" >> $log_file
-   sleep 2
+   sleep $sleep_interval
 
-	test -z "${is_sql_biz_as_usual_run:-}" && sleep 1 ; 
+	test -z "${is_sql_biz_as_usual_run:-}" || sleep 1 ; 
 	printf "\033[2J";printf "\033[0;0H"  ;    #and flush the screen
 	
 	doLog "INFO should run the following sql files: "
    echo -e "\n\n"
 	find "$sql_dir" -type f -name "*.sql"|sort -n
-	sleep 1
+   sleep $sleep_interval
 
 	# run the sql scripts in alphabetical order
-   while read -r sql_script ; do (
+   while read -r sql_script ; do 
 
 		relative_sql_script=$(echo $sql_script|perl -ne "s#$product_instance_dir##g;print")
 
 		# give the poor dev a time to see what is happening
-		test -z "${is_sql_biz_as_usual_run:-}" && sleep 1 ; 
+		test -z "${is_sql_biz_as_usual_run:-}" || sleep $sleep_interval ; 
 
 		# and clear the screen
 		printf "\033[2J";printf "\033[0;0H"
@@ -65,23 +70,25 @@ doRunPgsqlScripts(){
 		# run the sql save the result into a tmp log file
 		psql -v ON_ERROR_STOP=1 -q -t -X -U "${pgsql_user:-}" \
          -v db_name="$db_name" -f "$sql_script" "$db_name" > "$tmp_log_file" 2>&1
-       
+      ret=$?
+
+
 		# show the user what is happenning 
 		cat "$tmp_log_file"
-
-      test $ret -ne 0 && doExit $ret "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
-
+      test $ret -ne 0 && sleep 3
+      test $ret -ne 0 && export exit_code=1
+      test $ret -ne 0 && doExit "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
+      test $ret -ne 0 && break
 
 		# and save the tmp log file into the script log file
 		cat "$tmp_log_file" >> $log_file
 		echo -e '\n\n'
 
 		doLog "INFO STOP  ::: running $relative_sql_script"
-	);
 	done < <(find "$sql_dir" -type f -name "*.sql"|sort -n)
 	
 	doLog "INFO STOP  :: running sql scripts "	
-	test -z "${is_sql_biz_as_usual_run:-}" && sleep 1 ; 
+	test -z "${is_sql_biz_as_usual_run:-}" || sleep $sleep_interval ; 
 	
 	printf "\033[2J";printf "\033[0;0H"  ;    #and flush the screen
 	doLog "DEBUG STOP  doRunPgsqlScripts"
