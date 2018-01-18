fc07f3483723904ca5a15b4eb8dd80fd0cbd975f
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-01-18 22:34:57 +0200

    add MdlHsr2 initialization with Tests

---
+++ src/perl/issue_tracker/lib/IssueTracker/App/Mdl/MdlHsr2.pm
@@ -0,0 +1,113 @@
+use strict ; use warnings ; 
+package IssueTracker::App::Mdl::MdlHsr2 ; 
+
+   use Carp ; 
+
+   our $appConfig       = {} ; 
+   my $hsr2             = {} ; # this is the hash ref of hash refs 
+   my $mhsr2            = {} ; # and this is the meta hash ref of hash refs 
+   our $objLogger       = {} ; 
+
+
+	# -----------------------------------------------------------------------------
+	# the constructor 
+	# -----------------------------------------------------------------------------
+	sub new {
+
+		my $class      = shift;    # Class name is in the first parameter
+		$appConfig     = ${ shift @_ } || { 'foo' => 'bar' ,} ; 
+		$hsr2          = shift @_  || $hsr2 ; 
+		$mhsr2          = shift @_  || $mhsr2 ; 
+
+		my $self = {};        # Anonymous hash reference holds instance attributes
+		bless( $self, $class );    # Say: $self is a $class
+      $self = $self->doInitialize() ; 
+		return $self;
+	}  
+	#eof const
+	
+   #
+	# --------------------------------------------------------
+	# intializes this object 
+	# --------------------------------------------------------
+   sub doInitialize {
+      my $self = shift ; 
+
+      %$self = (
+           appConfig => $appConfig
+           , hsr2 => $hsr2
+           , mhsr2 => $mhsr2
+      );
+
+	   $objLogger 			= 'IssueTracker::App::Utils::Logger'->new( \$appConfig ) ;
+      
+
+      return $self ; 
+	}	
+	#eof sub doInitialize
+
+
+   #   
+   # -----------------------------------------------------------------------------
+	# return a field's value
+	# -----------------------------------------------------------------------------
+	sub get {
+
+		my $self = shift;
+		my $name = shift;
+		croak "\@TRYING to get an undef name" unless $name ;  
+		croak "\@TRYING to get an undefined value" unless ( $self->{"$name"} ) ; 
+
+		return $self->{ $name };
+	}    
+
+
+   #
+	# -----------------------------------------------------------------------------
+	# set a field's value
+	# -----------------------------------------------------------------------------
+	sub set {
+
+		my $self  = shift;
+		my $name  = shift;
+		my $value = shift;
+		$self->{ "$name" } = $value;
+	}
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+MdlHsr2
+
+=head1 SYNOPSIS
+
+use MdlHsr2 ; 
+
+
+=head1 DESCRIPTION
+the main model of the application : 
+passed between the readers, writers and the converters
+
+=head2 EXPORT
+
+
+=head1 SEE ALSO
+
+perldoc perlvars
+
+No mailing list for this module
+
+
+=head1 AUTHOR
+
+yordan.georgiev@gmail.com
+
+=head1 
+
+
+
+=cut 
