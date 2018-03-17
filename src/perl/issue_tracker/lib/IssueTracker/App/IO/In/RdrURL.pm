46a41b81e06b82ca75c4a811fa9803575382d52e
YordanGeorgiev <yordan.georgiev@gmail.com>
2018-03-18 00:15:13 +0200

    it-180317 copy the re-factored RdrFiles,RdrDirs, WtrFiles , WtrDirs into issue-handler

--- src/perl/issue_tracker/lib/IssueTracker/App/IO/In/RdrURL.pm
+++ src/perl/issue_tracker/lib/IssueTracker/App/IO/In/RdrURL.pm
@@ -17,7 +17,6 @@ package IssueTracker::App::IO::In::RdrURL;
 	use Carp qw /cluck confess shortmess croak carp/ ; 
    
    use base qw(IssueTracker::App::Utils::OO::SetGetable);
-   use IssueTracker::App::Utils::IO::FileHandler ; 
    use IssueTracker::App::Utils::Logger ;
    use WWW::Curl::Easy ; 
 	use Data::Printer ; 
