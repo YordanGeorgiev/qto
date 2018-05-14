44fe9a06575e5ed07fe309fb521aea77a5ec4122
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-05-14 19:38:50 +0300

    it-18051102 add simple dynamic and generic ok ctrl flow with vue

---
+++ src/perl/issue_tracker/lib/IssueTracker/App/UI/WtrUIFactory.pm
@@ -0,0 +1,85 @@
+package IssueTracker::App::UI::WtrUIFactory ; 
+
+	use strict; use warnings;
+	
+	use Data::Printer ; 
+   use Carp ; 
+
+   use IssueTracker::App::UI::Controls::WtrVueListLabelsTemplate ; 
+
+	our $appConfig 		= {} ; 
+	our $objModel        = {} ; 
+   our $ui_type         = 'pages' ; 
+
+	#
+	# -----------------------------------------------------------------------------
+	# fabricates different WtrDb object 
+	# -----------------------------------------------------------------------------
+	sub doInstantiate {
+
+		my $self 			= shift ; 	
+		my $ui_type			= shift // $ui_type ; # the default is control
+
+		my @args 			= ( @_ ) ; 
+
+		my $package_file  = () ; 
+		my $objWtrControl = () ;
+
+		if ( $ui_type eq 'Pages' ) {
+	      croak "not implemented !!!" ; 	
+		}
+		elsif ( $ui_type eq 'control/list-labels' ) {
+		   $package_file     = "IssueTracker/App/UI/Controls/WtrVueListLabelsTemplate.pm" ; 
+		   $objWtrControl    = "IssueTracker::App::UI::Controls::WtrVueListLabelsTemplate" ; 
+		}
+		else {
+	      croak "not implemented !!!" ; 	
+      }
+
+		require $package_file;
+
+		return $objWtrControl->new( \$appConfig , \$objModel , @args);
+	}
+	# eof sub doInstantiate
+	
+
+   #
+	# -----------------------------------------------------------------------------
+	# the constructor 
+	# -----------------------------------------------------------------------------
+	sub new {
+
+		my $invocant 			= shift ;    
+		$appConfig     = ${ shift @_ } || { 'foo' => 'bar' ,} ; 
+		$objModel      = ${ shift @_ } || croak 'missing objModel !!!' ; 
+      $ui_type          = shift || 'Pages' ; 
+		
+      # might be class or object, but in both cases invocant
+		my $class = ref ( $invocant ) || $invocant ; 
+
+		my $self = {};        # Anonymous hash reference holds instance attributes
+		bless( $self, $class );    # Say: $self is a $class
+      $self = $self->doInitialize() ; 
+		return $self;
+	}  
+	#eof const
+	
+   #
+	# --------------------------------------------------------
+	# intializes this object 
+	# --------------------------------------------------------
+   sub doInitialize {
+      my $self = shift ; 
+
+      %$self = (
+           appConfig => $appConfig
+      );
+
+      return $self ; 
+	}	
+	#eof sub doInitialize
+
+1;
+
+
+__END__
--- src/sql/pgsql/dev_issue_tracker/13.create-table-test_runs.sql
+++ src/sql/pgsql/dev_issue_tracker/13.create-table-test_runs.sql
@@ -9,14 +9,17 @@ SELECT 'create the "test_runs" table'
     , prio           integer NULL
     , files_num      integer NULL
     , status         varchar (50) NOT NULL
+    , test_num       varchar (20) NOT NULL
     , category       varchar (200) NOT NULL
     , name           varchar (200) NOT NULL
     , expected_result varchar (4000)
     , actual_result  varchar (4000)
     , log_output     varchar (4000)
-    , owner          varchar (50) NULL
+    , tester         varchar (50) NULL
     , start_time     text NULL
     , stop_time      text NULL
+    , duration_sec   decimal (6,2) NULL
+    , duration_min   decimal (6,2) NULL
     , update_time    timestamp DEFAULT NOW() 
     , CONSTRAINT pk_test_runs_guid PRIMARY KEY (guid)
     ) WITH (
