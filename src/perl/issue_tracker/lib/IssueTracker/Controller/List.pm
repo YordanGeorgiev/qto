21b330d4f595925c87ee383cfc4f427c5612e744
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-06-01 17:00:22 +0300

    it-1805242250 create a tag cloud page
    Former-commit-id: 12550cafa19081c8da4040edcddff736c7613121

--- src/perl/issue_tracker/lib/IssueTracker/Controller/List.pm
+++ src/perl/issue_tracker/lib/IssueTracker/Controller/List.pm
@@ -29,7 +29,8 @@ sub doListItems {
    my $rdbms_type       = 'postgres';
    my $ret              = 1 ; 
    my $msg              = '' ; 
-   my $vct_list_labels  = '' ; 
+   my $vct_list  = '' ; 
+   my $as               = 'lbls' ; 
 
 	print "List.pm ::: url: " . $self->req->url->to_abs . "\n\n" if $module_trace == 1 ; 
    $appConfig		 = $self->app->get('AppConfig');
@@ -46,8 +47,14 @@ sub doListItems {
    $objModel->set('list.web-action.hide' , $self->req->query_params->param('hide') );
    $objModel->set('list.web-action.o' , $self->req->query_params->param('o') );
 
-	# build the list control 
-   ( $ret , $msg , $vct_list_labels )  = $self->doBuildVueControlListLabels( $msg , \$objModel  ) ; 
+   $as = $self->req->query_params->param('as') || $as ; 
+   if ( $as eq 'lbls' ) {
+      ( $ret , $msg , $vct_list )  = $self->doBuildVueControlListLabels( $msg , \$objModel  ) ; 
+   } elsif ( $as eq 'cloud' ) {
+      ( $ret , $msg , $vct_list )  = $self->doBuildVueControlListCloud( $msg , \$objModel  ) ; 
+   } else {
+      ( $ret , $msg , $vct_list )  = $self->doBuildVueControlListLabels( $msg , \$objModel  ) ; 
+   }
 
    $msg = '<span id="spn_err_msg">' . $msg . '</span>' unless $ret == 0 ; 
    $msg = '<span id="spn_msg">' . $msg . '</span>' if $ret == 0 ; 
@@ -56,14 +63,71 @@ sub doListItems {
    $self->res->headers->accept_charset('UTF-8');
    $self->res->headers->accept_language('fi, en');
 
-   # and render the result
-   $self->render(
-      template          => 'controls/list'
-    , msg               => $msg
-    , item              => $item
-    , db 					=> $db
-    , vct_list_labels   => $vct_list_labels
-   ); 
+   if ( $as eq 'lbls' ) {
+      $self->render(
+         template          => 'controls/list'
+       , msg               => $msg
+       , item              => $item
+       , db 					=> $db
+       , vct_list   			=> $vct_list
+      ); 
+   } elsif ( $as eq 'cloud' ) {
+      $self->render(
+         template          => 'controls/cloud'
+       , msg               => $msg
+       , item              => $item
+       , db 					=> $db
+       , vct_list   			=> $vct_list
+      ); 
+   } else {
+      $self->render(
+         template          => 'controls/list'
+       , msg               => $msg
+       , item              => $item
+       , db 					=> $db
+       , vct_list   			=> $vct_list
+      ); 
+   } 
+
+}
+
+sub doBuildVueControlListCloud {
+
+	my $self          = shift ; 
+	my $msg           = shift ; 
+   my $objModel      = ${ shift @_ } ; 
+   my $ret           = 1 ; 
+   my $control       = '' ; 
+
+	# get the list of the table columns 
+   my $mhsr2 = {} ;
+   my $hsr2 = {} ;
+   my $objRdrDbsFactory
+      = 'IssueTracker::App::Db::In::RdrDbsFactory'->new(\$appConfig, \$objModel ) ;
+   my $objRdrDb = $objRdrDbsFactory->doInstantiate("$rdbms_type");
+   my $table = $objModel->get('table_name'); 
+
+   ( $ret , $msg , $mhsr2 ) = $objRdrDb->doSelectTablesColumnList ( $table ) ;
+   return ( $ret , $msg , '' ) unless $ret == 0 ; 
+
+	my $hsr2 = {};
+   my $objRdrDbsFactory
+      = 'IssueTracker::App::Db::In::RdrDbsFactory'->new(\$appConfig, \$objModel );
+   my $objRdrDb = $objRdrDbsFactory->doInstantiate("$rdbms_type");
+   ($ret, $msg) = $objRdrDb->doSelectTableIntoHashRef(\$objModel, $table);
+
+	my $objCnrHsr2ToArray =
+	'IssueTracker::App::Cnvr::CnrHsr2ToArray'->new ( \$appConfig , \$objModel ) ;
+		( $ret , $msg , $hsr2) = $objCnrHsr2ToArray->doConvert ($objModel->get('hsr2'));
+
+   my $objWtrUIFactory
+      = 'IssueTracker::App::UI::WtrUIFactory'->new(\$appConfig, \$objModel );
+   my $objUIBuilder = $objWtrUIFactory->doInstantiate('control/list-cloud');
+
+   ( $ret , $msg , $control ) = $objUIBuilder->doBuild() ; 
+
+   
+   return ( $ret , $msg , $control ) ; 
 }
 
 sub doBuildVueControlListLabels {
