941484af4e9f17ed0e7e97ac3bf1a1ec45ced184
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-10-09 21:59:00 +0300

    it-181009195756 add the requirements and userstories tables ddls
    Former-commit-id: 962987e8edfebad59a89a343e550d4610665db18

--- src/sql/pgsql/prd_issue_tracker/13.create-table-requirements.sql
+++ src/sql/pgsql/prd_issue_tracker/13.create-table-requirements.sql
@@ -1,23 +1,26 @@
--- DROP TABLE IF EXISTS check_lists ; 
+-- DROP TABLE IF EXISTS requirements ; 
 
-SELECT 'create the "check_lists" table'
+SELECT 'create the "requirements" table'
 ; 
-   CREATE TABLE check_lists (
+   CREATE TABLE requirements (
       guid           UUID NOT NULL DEFAULT gen_random_uuid()
     , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
-    , seq            integer NULL
     , prio           integer NOT NULL DEFAULT 1
+    , weight         integer NOT NULL DEFAULT 9
     , status         varchar (50) NOT NULL DEFAULT 'type the status ...'
     , category       varchar (200) NOT NULL DEFAULT 'type the category ...'
     , name           varchar (200) NOT NULL DEFAULT 'type the name ...'
     , description    varchar (4000)
+    , owner          varchar (50) NOT NULL DEFAULT 'unknown'
+    , tags           varchar (200)
     , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
-    , CONSTRAINT pk_check_lists_guid PRIMARY KEY (guid)
+    , CONSTRAINT pk_requirements_guid PRIMARY KEY (guid)
     ) WITH (
       OIDS=FALSE
     );
 
-create unique index idx_uniq_check_lists_id on check_lists (id);
+create unique index idx_uniq_requirements_id on requirements (id);
+
 
 
 SELECT 'show the columns of the just created table'
@@ -25,14 +28,17 @@ SELECT 'show the columns of the just created table'
 
    SELECT attrelid::regclass, attnum, attname
    FROM   pg_attribute
-   WHERE  attrelid = 'public.check_lists'::regclass
+   WHERE  attrelid = 'public.requirements'::regclass
    AND    attnum > 0
    AND    NOT attisdropped
    ORDER  BY attnum
    ; 
 
+--The trigger:
+CREATE TRIGGER trg_set_update_time_on_requirements BEFORE UPDATE ON requirements FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
 
 select tgname
 from pg_trigger
 where not tgisinternal
-and tgrelid = 'check_lists'::regclass;
+and tgrelid = 'requirements'::regclass;
+
