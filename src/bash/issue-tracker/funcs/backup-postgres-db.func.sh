b2d65427b9e5b1d1bb1718d81cf3d08a0313d09e
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-10-09 20:13:36 +0300

    it-181009151205 add default db backup, if shell vars not loaded

--- src/bash/issue-tracker/funcs/backup-postgres-db.func.sh
+++ src/bash/issue-tracker/funcs/backup-postgres-db.func.sh
@@ -8,6 +8,10 @@ doBackupPostgresDb(){
 
 	doLog "DEBUG START doBackupPostgresDb"
 	# Action !!!
+   test -z "${proj_instance_dir-}" && proj_instance_dir="$product_instance_dir"
+   test -z "${mix_data_dir-}" && mix_data_dir="$proj_instance_dir/dat/mix"
+
+   test -z "${postgres_db_name-}" && postgres_db_name="${env_type-}"_"${run_unit//-/_}"
    mkdir -p $mix_data_dir/$(date "+%Y")/$(date "+%Y-%m")/$(date "+%Y-%m-%d")/sql/$postgres_db_name/; 
    pg_dump  --column-inserts --data-only $postgres_db_name  > \
    $mix_data_dir/$(date "+%Y")/$(date "+%Y-%m")/$(date "+%Y-%m-%d")/sql/$postgres_db_name/$postgres_db_name.`date "+%Y%m%d_%H%M%S"`.insrt.dmp.sql 
