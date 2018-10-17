a45d2faa5502e7c5df9fff5915312c369806a3b1
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-10-17 21:47:50 +0300

    it-181017214639 restrict the amount of tables in the select tests
    Former-commit-id: 0ddd055dcd5e305289b10dd2c0f4ec423c4b580a

--- src/perl/issue_tracker/t/lib/IssueTracker/Controller/TestSelectFilter.pl
+++ src/perl/issue_tracker/t/lib/IssueTracker/Controller/TestSelectFilter.pl
@@ -24,6 +24,7 @@ my $tm = '' ;
 
 $res = $ua->get('/' . $db_name . '/select-tables')->result->json ; 
 my $list = $res->{'dat'} ; 
+my @tables_to_check = ( 'monthly_issues' , 'yearly_issues' ) ; 
 
    
 
@@ -31,6 +32,7 @@ my $list = $res->{'dat'} ;
 for my $row ( @$list ) {
 
    my $table = $row->{'table_name'} ; 
+   next unless ( grep( /^$table$/, @tables_to_check ) ) ;
 	my $url_params = '' ; # 
 	my $url = '' ; 
 
