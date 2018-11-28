675d63e31c5ae5a4dc37ef0a539fd8a668440cf9
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-11-28 22:47:54 +0200

    it-181127215257 stop_time-start_time diff trigger
    Former-commit-id: de34e5feca720762f0dfa9ffb9296318410a541f

--- src/sql/pgsql/tst_issue_tracker/93.create-table-benchmarks.sql
+++ src/sql/pgsql/tst_issue_tracker/93.create-table-benchmarks.sql
@@ -1,17 +1,22 @@
--- DROP TABLE IF EXISTS benchmarks ; 
+DROP TABLE IF EXISTS benchmarks ; 
 
 SELECT 'create the "benchmarks" table'
 ; 
    CREATE TABLE benchmarks (
       guid           UUID NOT NULL DEFAULT gen_random_uuid()
     , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
-    , git_hash       char (8) NULL DEFAULT ''
-    , name           varchar (200) NOT NULL DEFAULT 'name ...'
-    , url            varchar (4000) NOT NULL DEFAULT 'url ...'
-    , start_time     timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
-    , stop_time      timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
-    , avg_ua_load_time numeric( 6,6 ) NOT NULL  default '0.0' 
-    , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
+    , git_hash                char (8) NULL DEFAULT ''
+    , name                    varchar (200) NOT NULL DEFAULT 'name ...'
+    , url                     varchar (4000) NOT NULL DEFAULT 'url ...'
+    , start_time              timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
+    , stop_time               timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
+    , avg_ua_load_time        numeric( 6,6 ) NOT NULL  default '0.0' 
+    , diff_time               varchar (20) NOT NULL DEFAULT 'HH:MI:SS'
+    , diff_min                numeric ( 10,2 ) NOT NULL  default '0.0' 
+    , diff_sec                numeric ( 14,2 ) NOT NULL  default '0.0' 
+    , diff_hours              numeric ( 10,2 ) NOT NULL  default '0.0' 
+    , description             varchar (4000) NOT NULL DEFAULT 'desc...'
+    , update_time             timestamp DEFAULT DATE_TRUNC('second', NOW())
     , CONSTRAINT pk_benchmarks_guid PRIMARY KEY (guid)
     ) WITH (
       OIDS=FALSE
@@ -32,8 +37,86 @@ SELECT 'show the columns of the just created table'
    ORDER  BY attnum
    ; 
 
---The trigger:
-CREATE TRIGGER trg_set_update_time_on_benchmarks BEFORE UPDATE ON benchmarks FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+-- START trigger trg_benchmarks_upsrt_update_time
+CREATE TRIGGER trg_benchmarks_upsrt_update_time 
+   BEFORE UPDATE ON benchmarks 
+   FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+
+-- STOP trigger trg_benchmarks_upsrt_update_time
+
+-- START trigger trg_benchmarks_upsrt_diff_time
+-- hrt = human readable time
+CREATE OR REPLACE FUNCTION fnc_benchmarks_upsrt_diff_time()
+
+    RETURNS TRIGGER
+    AS $$
+       BEGIN 
+          -- NEW.diff_time = age(NEW.stop_time::timestamp-NEW.start_time::timestamp);
+          NEW.diff_time = to_char(NEW.stop_time::timestamp-NEW.start_time::timestamp, 'DDD HH24:MI:SS');
+          RETURN NEW;
+       END;
+    $$ LANGUAGE plpgsql;
+
+
+ CREATE TRIGGER trg_benchmarks_upsrt_diff_time
+  BEFORE INSERT OR UPDATE ON benchmarks
+  FOR EACH ROW EXECUTE PROCEDURE fnc_benchmarks_upsrt_diff_time();
+-- 
+-- STOP trigger trg_benchmarks_upsrt_diff_time
+
+-- START trg_benchmarks_upsrt_diff_sec
+CREATE OR REPLACE FUNCTION fnc_benchmarks_calc_diff_sec()
+
+    RETURNS TRIGGER
+    AS $$
+       BEGIN 
+          NEW.diff_sec = EXTRACT(EPOCH FROM (NEW.stop_time::timestamp - NEW.start_time::timestamp));
+          RETURN NEW;
+       END;
+    $$ LANGUAGE plpgsql;
+
+ CREATE TRIGGER trg_benchmarks_upsrt_diff_sec
+  BEFORE INSERT OR UPDATE ON benchmarks
+  FOR EACH ROW EXECUTE PROCEDURE fnc_benchmarks_calc_diff_sec();
+-- STOP trg_benchmarks_upsrt_diff_sec
+
+-- START trg_benchmarks_upsrt_diff_hours
+CREATE OR REPLACE FUNCTION fnc_benchmarks_calc_diff_hours()
+
+    RETURNS TRIGGER
+    AS $$
+       BEGIN 
+          NEW.diff_hours = 
+(DATE_PART('day', NEW.stop_time::timestamp - NEW.start_time::timestamp) * 24 + 
+DATE_PART('hour', NEW.stop_time::timestamp - NEW.start_time::timestamp))  +
+DATE_PART('minute', NEW.stop_time::timestamp - NEW.start_time::timestamp)/60;
+          RETURN NEW;
+       END;
+    $$ LANGUAGE plpgsql;
+
+ CREATE TRIGGER trg_benchmarks_upsrt_diff_hours
+  BEFORE INSERT OR UPDATE ON benchmarks
+  FOR EACH ROW EXECUTE PROCEDURE fnc_benchmarks_calc_diff_hours();
+-- STOP trg_benchmarks_upsrt_diff_hours
+
+-- START trg_benchmarks_upsrt_diff_min
+CREATE OR REPLACE FUNCTION fnc_benchmarks_calc_diff_min()
+
+    RETURNS TRIGGER
+    AS $$
+       BEGIN 
+          NEW.diff_min = 
+(DATE_PART('day', NEW.stop_time::timestamp - NEW.start_time::timestamp) * 24 * 60 + 
+DATE_PART('hour', NEW.stop_time::timestamp - NEW.start_time::timestamp)) * 60 +
+DATE_PART('minute', NEW.stop_time::timestamp - NEW.start_time::timestamp);
+          RETURN NEW;
+       END;
+    $$ LANGUAGE plpgsql;
+
+ CREATE TRIGGER trg_benchmarks_upsrt_diff_min
+  BEFORE INSERT OR UPDATE ON benchmarks
+  FOR EACH ROW EXECUTE PROCEDURE fnc_benchmarks_calc_diff_min();
+-- STOP trg_benchmarks_upsrt_diff_min
 
 select tgname
 from pg_trigger
