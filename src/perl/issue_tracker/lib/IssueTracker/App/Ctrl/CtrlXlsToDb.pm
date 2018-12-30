425db62fd8df32e40ae9ba0f3f9c0e45dcafa99a
Yordan Georgiev <yordan.georgiev@gmail.com>
2018-12-30 22:36:59 +0200

    it-181216141631 release v0.5.6 "read-only docs"

--- src/perl/issue_tracker/lib/IssueTracker/App/Ctrl/CtrlXlsToDb.pm
+++ src/perl/issue_tracker/lib/IssueTracker/App/Ctrl/CtrlXlsToDb.pm
@@ -75,7 +75,7 @@ package IssueTracker::App::Ctrl::CtrlXlsToDb ;
 	   push ( @tables , split(',',$tables ) ) ; 
       my $xls_file            = '' ; 
      
-      print "using the following \@tables: " . join (",", @tables) . "\n" ; 
+      print STDOUT "using the following \@tables: " . join (",", @tables) . "\n" ; 
       # if the xls_file is not defined take the latest one from the mix data dir
       if ( $objModel->get( 'io.xls-file' ) eq 'undefined' ) {
          my $xls_dir          = $ENV{'mix_data_dir' } ;  ; 
