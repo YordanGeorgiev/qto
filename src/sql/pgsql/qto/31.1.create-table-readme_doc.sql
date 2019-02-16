b737c84ded7021fa9e5166579359fd420a9fa9d2
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-02-16 12:10:21 +0200

    it-190202124512 add the readme doc table
    Former-commit-id: cdebee1613b8b9292ed973fc5b0ccd258723fed9

---
+++ src/sql/pgsql/qto/31.1.create-table-readme_doc.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS readme_doc CASCADE;
+
+SELECT 'create the "readme_doc" table'
+; 
+   CREATE TABLE readme_doc (
+      guid           UUID NOT NULL DEFAULT gen_random_uuid()
+    , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
+    , level          integer NULL
+    , seq            integer NULL
+    , lft            bigint  NULL
+    , rgt            bigint  NULL
+    , prio           integer NOT NULL DEFAULT 1
+    , status         varchar (20) NOT NULL DEFAULT 'status ...'
+    , name           varchar (100) NOT NULL DEFAULT 'name ...'
+    , description    varchar (4000)
+    , src            varchar (4000)
+    , formats  text NULL
+    , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
+    , CONSTRAINT pk_readme_doc_guid PRIMARY KEY (guid)
+    ) WITH (
+      OIDS=FALSE
+    );
+
+   create unique index idx_uniq_readme_doc_id on readme_doc (id);
+
+
+
+SELECT 'show the columns of the just created table'
+; 
+
+   SELECT attrelid::regclass, attnum, attname
+   FROM   pg_attribute
+   WHERE  attrelid = 'public.readme_doc'::regclass
+   AND    attnum > 0
+   AND    NOT attisdropped
+   ORDER  BY attnum
+   ; 
+
+--The trigger:
+   CREATE TRIGGER trg_set_update_time_on_readme_doc 
+   BEFORE UPDATE ON readme_doc 
+   FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+
+select tgname
+from pg_trigger
+where not tgisinternal
+and tgrelid = 'readme_doc'::regclass;
+
---
+++ src/sql/pgsql/qto/31.create-table-readme.sql
@@ -0,0 +1,42 @@
+   DROP TABLE IF EXISTS readme CASCADE;
+
+SELECT 'create the "readme" table'
+; 
+   CREATE TABLE readme (
+      guid           UUID NOT NULL DEFAULT gen_random_uuid()
+    , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
+    , prio           integer NOT NULL DEFAULT 1
+    , status         varchar (20) NOT NULL DEFAULT 'status ...'
+    , name           varchar (100) NOT NULL DEFAULT 'name ...'
+    , description    varchar (4000)
+    , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
+    , CONSTRAINT pk_readme_guid PRIMARY KEY (guid)
+    ) WITH (
+      OIDS=FALSE
+    );
+
+   create unique index idx_uniq_readme_id on readme (id);
+
+
+
+SELECT 'show the columns of the just created table'
+; 
+
+   SELECT attrelid::regclass, attnum, attname
+   FROM   pg_attribute
+   WHERE  attrelid = 'public.readme'::regclass
+   AND    attnum > 0
+   AND    NOT attisdropped
+   ORDER  BY attnum
+   ; 
+
+--The trigger:
+   CREATE TRIGGER trg_set_update_time_on_readme 
+   BEFORE UPDATE ON readme 
+   FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+
+select tgname
+from pg_trigger
+where not tgisinternal
+and tgrelid = 'readme'::regclass;
+
