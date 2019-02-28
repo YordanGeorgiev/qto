2bf97cc680f0af6ea4c89f1106361a318865205f
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-02-28 22:07:17 +0200

    it-190228213134 the remove-action-files did not remove the relative paths bug fix

---
+++ src/bash/issue-tracker/tests/action-to-remove.test.sh
@@ -0,0 +1,22 @@
+# src/bash/issue-tracker/funcs/action-to-remove.test.sh
+
+# v1.0.9
+# ---------------------------------------------------------
+# todo: add doTestActionToRemove comments ...
+# ---------------------------------------------------------
+doTestActionToRemove(){
+
+	doLog "DEBUG START doTestActionToRemove"
+	
+	cat doc/txt/issue-tracker/tests/action-to-remove.test.txt
+	
+	sleep "$sleep_interval"
+	# add your action implementation code here ... 
+	# Action !!!
+
+	doLog "DEBUG STOP  doTestActionToRemove"
+}
+# eof func doTestActionToRemove
+
+
+# eof file: src/bash/issue-tracker/funcs/action-to-remove.test.sh
