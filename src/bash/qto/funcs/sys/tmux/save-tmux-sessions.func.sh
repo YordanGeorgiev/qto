43cf98daf146428124b84989522fb3ba8c2d4eb6
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-05-29 23:03:02 +0300

    qto-190517101142 stop  v0.6.3
    Former-commit-id: d336b47de4265c197b170ad5caa324fa541bc8a6

--- src/bash/qto/funcs/sys/tmux/save-tmux-sessions.func.sh
+++
@@ -1,7 +0,0 @@
-doSaveTmuxSessions(){
-  source "$product_instance_dir/src/bash/qto/funcs/sys/tmux/tmux-common.sh"
-  doCheckTmuxIsInstalled
-  local d=$'\t'
-  tmux list-windows -a -F "#S${d}#W${d}#{pane_current_path}" > ~/.tmux-session
-}
-
