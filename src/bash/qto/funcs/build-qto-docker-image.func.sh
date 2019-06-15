2a42ad6110f777e9f38adc87753e7eb6ab2251ab
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-06-15 11:18:02 +0300

    qto-190615111546 add docker images and containers cleaning shell actions

--- src/bash/qto/funcs/build-qto-docker-image.func.sh
+++ src/bash/qto/funcs/build-qto-docker-image.func.sh
@@ -8,15 +8,16 @@ doBuildQtoDockerImage(){
 
 	doLog "DEBUG START doBuildQtoDockerImage"
 	
-	sleep "$sleep_interval"
-	# add your action implementation code here ... 
-	# Action !!!
-   cd $product_instance_dir/src/docker
-   docker build -t qto-image .
-   cd -
+   img_file_name='ubuntu-bionic-core-cloudimg-amd64-root.tar.gz'
+   mkdir -p $product_instance_dir/dat/tar/
+   uri='https://partner-images.canonical.com/core/bionic/current/'"$img_file_name"
+   test -f $product_instance_dir/dat/tar/$img_file_name || \
+      wget -O "$product_instance_dir/dat/tar/$img_file_name" "$uri"
+
+   docker build -t qto-image -f "$product_instance_dir/src/docker/Dockerfile" .
 
    # run the docker in the background
-   docker run -d --name  qto-cotainer-01 -p 127.0.0.1:15432:15432 --restart=always qto-image
+   docker run -d --name  qto-container-01 -p 127.0.0.1:15432:15432 --restart=always qto-image
 
 	# --detach , -d       Run container in background and print container ID
 	# --name              Assign a name to the container
---
+++ src/bash/qto/funcs/remove-all-docker-images.func.sh
@@ -0,0 +1,25 @@
+# file: src/bash/qto/funcs/remove-all-docker-images.sh
+
+# v0.6.5
+# ---------------------------------------------------------
+# remove all the docker images on the host with warning
+# and inform if none exists
+# ---------------------------------------------------------
+doRemoveAllDockerImages(){
+   clearTheScreen
+	doLog "INFO START doRemoveAllDockerImages !!! " 
+   doLog "INFO Are you SURE ??!! You have 3 seconds to abort by Ctrl + C !!"
+   sleep 3
+   
+   if test -z "$(docker images | grep '^<none>' | awk '{print $3}')"
+   then
+      doLog "INFO No docker images found. Nothing to do !!!"
+   else
+      echo docker rmi $(docker images | grep '^<none>' | awk '{print $3}')
+   fi
+
+	doLog "INFO STOP  doRemoveAllDockerImages"
+}
+
+
+# eof file: src/bash/qto/funcs/remove-all-docker-images.sh
