1255d44a9e09bac58c9c9e52cd35ee993e3adf48
Yordan Georgiev <yordan.georgiev@phz.fi>
2019-06-27 13:32:56 +0300

    qto-190627133221 clean-up obsolete save-tmux-session action files

--- src/bash/qto/bootstrap-qto.sh
+++ src/bash/qto/bootstrap-qto.sh
@@ -44,15 +44,6 @@ sudo ln -s "$tgt_path" "$link_path"
 ls -la $link_path; 
 # STOP === create symlink
 
-export to_srch='ysg'
-export to_repl=$run_unit_owner
-export dir=$product_instance_dir/cnf
-#-- search and replace in file and dir paths excludding the .git dir
-find "$dir/"  -not -path "./.git/*" -type d |\
-perl -nle '$o=$_;s#'"$to_srch"'#'"$to_repl"'#g;$n=$_;`mkdir -p $n` ;'
-find "$dir/"  -not -path "./.git/*" -type f |\
-perl -nle '$o=$_;s#'"$to_srch"'#'"$to_repl"'#g;$n=$_;rename($o,$n) unless -e $n ;'
-
 #-- search and replace in file contents excludging the .git dir
 find "$dir/" -not -path "./.git/*" -type f -exec perl -pi -e "s#$to_srch#$to_repl#g" {} \;
 find "$dir/" -not -path "./.git/*" -type f -name '*.bak' | xargs rm -f
@@ -68,16 +59,16 @@ while read -r f ; do sudo perl -pi -e 's/'"$to_srch"'/'"$to_repl"'/g' $f ; \
 
 # set a default .vimrc
 test -f ~/.vimrc && cp -v ~/.vimrc ~/.vimrc.$(date "+%Y%m%d_%H%M%S").bak
-cp -v $product_instance_dir/cnf/hosts/$(hostname -s)/home/$run_unit_owner/.vimrc ~/.vimrc
+cp -v $(find $product_instance_dir/cnf/hosts/ -name '.vimrc') ~/.vimrc
 
 psql_cnf_dir='/etc/postgresql/11/main'
 test -f $psql_cnf_dir/pg_hba.conf && \
-   sudo cp -v $psql_cnf_dir/pg_hba.conf $psql_cnf_dir/pg_hba.conf.orig && \
-   sudo cp -v $product_instance_dir/cnf/hosts/$(hostname -s)"$psql_cnf_dir"/pg_hba.conf $psql_cnf_dir/pg_hba.conf
+   sudo cp -v $psql_cnf_dir/pg_hba.conf $psql_cnf_dir/pg_hba.conf.orig.bak && \
+   sudo cp -v $(find $product_instance_dir/cnf -name pg_hba.conf) $psql_cnf_dir/pg_hba.conf
 
 test -f $psql_cnf_dir/postgresql.conf && \
    sudo cp -v $psql_cnf_dir/postgresql.conf $psql_cnf_dir/postgresql.conf.orig && \
-   sudo cp -v $product_instance_dir/cnf/hosts/$(hostname -s)"$psql_cnf_dir"/postgresql.conf $psql_cnf_dir/postgresql.conf
+   sudo cp -v $(find $product_instance_dir/cnf -name postgresql.conf) $psql_cnf_dir/postgresql.conf
 
 echo "DO ALWAYS ! GO TO YOUR product_instance_dir by while working on a qto instance !!!"
 echo "cd $product_instance_dir"
