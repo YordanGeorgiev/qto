a3f00e00555e5b1cdda8ecac7b479353908ed089
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-07-05 19:12:57 +0300

    qto-190627214311 fix bug for the pager is visible if the table is not found and it should not

--- src/sql/pgsql/qto/91.create-table-test_empty_table.sql
+++ src/sql/pgsql/qto/91.create-table-test_empty_table.sql
@@ -1,4 +1,4 @@
---DROP TABLE IF EXISTS test_empty_table ; 
+-- DROP TABLE IF EXISTS test_empty_table ; 
 
 SELECT 'create the "test_empty_table" table'
 ;
@@ -6,7 +6,7 @@ SELECT 'create the "test_empty_table" table'
    CREATE TABLE test_empty_table (
       guid           UUID NOT NULL DEFAULT gen_random_uuid()
     , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
-    , name           varchar (200) NOT NULL
+    , name           varchar (200) NOT NULL DEFAULT 'name...'
     , description    varchar (4000)
     , CONSTRAINT pk_test_empty_table_guid PRIMARY KEY (guid)
     ) WITH (
