ab936ab41b80ce32f7ca592233e4e3feee5e3757
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-07-08 16:55:59 +0300

    qto-190708165448 add check for email validity during logon in back-end

--- src/perl/qto/t/lib/Qto/Controller/TestLogon.pl
+++ src/perl/qto/t/lib/Qto/Controller/TestLogon.pl
@@ -21,23 +21,27 @@ BEGIN { unshift @INC, "$FindBin::Bin/../../../../../qto/lib" }
 
    # 04..06
    $tm = "the email posted should not be undefined"; 
-   ok ( $t->post_ok($url => json => {"email" =>undef})->status_is(400) , $tm );
+   ok ( $t->post_ok($url => json => {"email"=>undef})->status_is(400) , $tm );
   
    # 07..09
    $tm = "the password posted should not be empty "; 
    ok ( $t->post_ok($url => json => {"email" =>'email', 'password' => ''})->status_is(400) , $tm );
  
-   #10..12
+   # 10..12
    $tm = "the password posted should not be undefined"; 
    ok ( $t->post_ok($url => json => {"email" =>'email', 'password' => undef})->status_is(400) , $tm );
    
-   #13..15
-   $tm = "both email and password are filled in and the password matches - see users create table";
-   ok ( $t->post_ok($url => json => {"email" =>'test.user@gmail.com', 'password' => 'password'})->status_is(200) , $tm );
+   # 13..15
+   $tm = "400 unauthorized is returned if the email is not valid"; 
+   ok ( $t->post_ok($url => json => {"email" =>'test.user@gmail', 'password' => 'invalid'})->status_is(400) , $tm );
 
-   #16..18
+   # 16..18
    $tm = "401 unauthorized is returned if the password does not match"; 
    ok ( $t->post_ok($url => json => {"email" =>'test.user@gmail.com', 'password' => 'invalid'})->status_is(401) , $tm );
+
+   # 19..21
+   $tm = "both email and password are filled in and the password matches - see users create table";
+   ok ( $t->post_ok($url => json => {"email" =>'test.user@gmail.com', 'password' => 'password'})->status_is(200) , $tm );
 
 
 done_testing();
