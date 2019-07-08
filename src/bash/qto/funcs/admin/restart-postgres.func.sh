4f71efb9a24a320daaa6dc38a2feaa308db1049e
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-07-08 11:57:13 +0300

    qto-190708010226 fix the too many open files bug for the data load select test

--- src/bash/qto/funcs/admin/restart-postgres.func.sh
+++ src/bash/qto/funcs/admin/restart-postgres.func.sh
@@ -4,6 +4,11 @@ doRestartPostgres(){
 	test `sudo -n -l -U $USER 2>&1 | egrep -c -i "not allowed to run sudo|unknown user"` -eq 0 \
 		|| doLog "FATAL you need to have sudo to run this script !!!"
 
+  
+   # to prevent the 'failed: could not create socket: Too many open files at sys' error
+   # perl -e 'while(1){open($a{$b++}, "<" ,"/dev/null") or die $b;print " $b"}' to check
+   ulimit -Sn 4096
+
    doLog "INFO start restarting postgres ..."
    sudo sh /etc/init.d/postgresql restart
    doLog "INFO stop  restarting postgres ..."
