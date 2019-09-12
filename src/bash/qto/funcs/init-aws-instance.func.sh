f8e66da31a0f59713964aa2055bbb40df266af2c
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-09-12 23:47:25 +0300

    qto-190912231424 add terraform ini aws instance for clean bionic 18.04 instance init
    Former-commit-id: bfb7106d420c1b9578b3f491cb44f791a45387cf

--- src/bash/qto/funcs/init-aws-instance.func.sh
+++ src/bash/qto/funcs/init-aws-instance.func.sh
@@ -1,31 +1,36 @@
 doInitAwdInstance(){
 
-   # attempt to install terraform, if it is missing !!! Obs needs sudo !!!
-   which terraform  2>/dev/null || { 
-      ver='0.12.8'
-      wget -O "/tmp/terraform_""$ver'_linux_amd64.zip" \
-         "https://releases.hashicorp.com/terraform/$ver/terraform_""$ver"'_linux_amd64.zip'
-      sudo unzip -o "/tmp/terraform_""$ver'_linux_amd64.zip" -d '/usr/local/bin'
-      terraform -V
-   }
+   doCheckInitTerraform 
 
-   which terraform  2>/dev/null || { 
-      echo >&2 "The terraform binary is missing ! Aborting ..."; exit 1; }
-   
    test -z "${PROJ_INSTANCE_DIR-}" && PROJ_INSTANCE_DIR="$PRODUCT_INSTANCE_DIR"
    source $PROJ_INSTANCE_DIR/.env ; env_type=$ENV_TYPE
    doExportJsonSectionVars $PROJ_INSTANCE_DIR/cnf/env/$env_type.env.json '.env.aws'
-   cp -v $PRODUCT_INSTANCE_DIR/src/terraform/tpl/csitea/main.tf.tpl \
-      $PRODUCT_INSTANCE_DIR/src/terraform/csitea/main.tf
+   mkdir -p $PRODUCT_INSTANCE_DIR/src/terraform/qto
+   cp -v $PRODUCT_INSTANCE_DIR/src/terraform/tpl/qto/main.tf.tpl \
+      $PRODUCT_INSTANCE_DIR/src/terraform/qto/main.tf
 
-	main_tf_file="$PRODUCT_INSTANCE_DIR/src/terraform/csitea/main.tf"   
+	public_key=$(cat echo $public_key_path)
+	main_tf_file="$PRODUCT_INSTANCE_DIR/src/terraform/qto/main.tf"   
+
+
+	# search and replace the variables from the configuration file to the tpl file
    perl -pi -e 's|\$access_key|'"$access_key"'|g' "$main_tf_file"
    perl -pi -e 's|\$secret_key|'"$secret_key"'|g' "$main_tf_file"
+   perl -pi -e 's|\$ENV_TYPE|'"$ENV_TYPE"'|g' "$main_tf_file"
+   perl -pi -e 's|\$aws_region|'"$aws_region"'|g' "$main_tf_file"
+   perl -pi -e 's|\$availability_zone|'"$availability_zone"'|g' "$main_tf_file"
+   perl -pi -e 's|\$ssh_key_pair_file|'"$ssh_key_pair_file"'|g' "$main_tf_file"
+   perl -pi -e 's|\$public_key_path|'"$public_key_path"'|g' "$main_tf_file"
+   perl -pi -e 's|\$public_key|'"$public_key"'|g' "$main_tf_file"
+   perl -pi -e 's|\$key_name|'"$key_name"'|g' "$main_tf_file"
 
-   cd $PRODUCT_INSTANCE_DIR/src/terraform/csitea/
+   cd $PRODUCT_INSTANCE_DIR/src/terraform/qto/
    terraform init
    terraform plan
 	test $? -ne 0 && doExit 1 "terraform plan failed"
+	
+	# import the deployment key first ?!
+	# nope terraform import aws_key_pair."aws_key_pair-$ENV_TYPE" "$key_name"
 	terraform apply -auto-approve
 	
 	rm -v "$main_tf_file"
