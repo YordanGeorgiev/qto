feaa14180be76332a342ddc073657ecc9dbf4b76
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-09-20 19:23:37 +0300

    qto-190920192226 add infra description in the system_guide_doc

--- doc/cheats/qto-cheat-sheet.sh
+++ doc/cheats/qto-cheat-sheet.sh
@@ -1,5 +1,11 @@
 # file: doc/cheats/qto-cheat-sheet.sh
 
+# how-to export the documentation of the desired db. 
+clear ; bash src/bash/qto/qto.sh -a generate-md-docs
+
+# how-to load xls sheets into tables , obs - do_truncate_tables=1 WILL truncate the postgres tables
+export do_truncate_tables=0 ; clear ; perl src/perl/qto/script/qto.pl --do xls-to-db --tables system_guide_doc
+
 # source the env vars loading function:
 source lib/bash/funcs/export-json-section-vars.sh
 
@@ -10,11 +16,10 @@ export PROJ_INSTANCE_DIR=`pwd`
 doExportJsonSectionVars $PROIJ_INSTANCE_DIR/cnf/env/prd.env.json '.env.db'
 
 # how to add files to the git 
-git add --all; GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+git add --all; GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" \
 git commit -m "$git_msg" --author "Yordan Georgiev <yordan.georgiev@gmail.com" # --amend
-
-# and push them
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" git push
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" git pull
 
 # set the authentication for the psql 
 alias psql="PGPASSWORD=${postgres_db_useradmin_pw:-} psql -v -t -X -w -U ${postgres_db_useradmin:-} --host $postgres_db_host --port $postgres_db_port"
