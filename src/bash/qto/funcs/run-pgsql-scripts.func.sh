6de101a7b140cdef508110fbea4e435bf393cbfa
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-09-21 09:50:15 +0300

    qto-190921094706 fix bug not passing correctly the userapp role pw to run-pgsql-scripts
    Former-commit-id: 2e8aa008067d342cc74cb2eff862f78f0ac33470

--- src/bash/qto/funcs/run-pgsql-scripts.func.sh
+++ src/bash/qto/funcs/run-pgsql-scripts.func.sh
@@ -2,14 +2,18 @@
 # cat doc/txt/qto/funcs/run-pgsql-scripts.func.txt
 # ---------------------------------------------------------
 doRunPgsqlScripts(){
-
-   test -z "${PROJ_INSTANCE_DIR-}" && PROJ_INSTANCE_DIR="$PRODUCT_INSTANCE_DIR"
+   
+   # the multi-project , multi-db "magic" is here ...
+   test -z "${PROJ_INSTANCE_DIR-}" && export PROJ_INSTANCE_DIR="$PRODUCT_INSTANCE_DIR"
    source $PROJ_INSTANCE_DIR/.env ; env_type=$ENV_TYPE
    test -z ${PROJ_CONF_FILE:-} && export PROJ_CONF_FILE="$PROJ_INSTANCE_DIR/cnf/env/$env_type.env.json"
    doExportJsonSectionVars $PROJ_CONF_FILE '.env.db'
-   export tmp_log_file="$tmp_dir/.$$.log"
+   doLog "using PROJ_INSTANCE_DIR: $PROJ_INSTANCE_DIR"
+   doLog "using PROJ_CONF_FILE: $PROJ_CONF_FILE"
+   tmp_log_file="$tmp_dir/.$$.log"
+   sleep 3
 
-   # create the db
+   # 00 create the db
 	pgsql_scripts_dir="$PRODUCT_INSTANCE_DIR/src/sql/pgsql/qto"
    sql_script="$pgsql_scripts_dir/""00.create-db.pgsql"
    PGPASSWORD="${postgres_db_useradmin_pw:-}" psql -v -q -t -X -w -U "${postgres_db_useradmin:-}" \
@@ -18,13 +22,12 @@ doRunPgsqlScripts(){
 		-v postgres_db_name="${postgres_db_name:-}" \
       -f "$sql_script" postgres > "$tmp_log_file" 2>&1
    ret=$?
-   cat "$tmp_log_file" # show it 
+   cat "$tmp_log_file" ; cat "$tmp_log_file" >> $log_file # show it and save it 
    test $ret -ne 0 && sleep 3
    test $ret -ne 0 && doExit 1 "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
    test $ret -ne 0 && break
-	cat "$tmp_log_file" >> $log_file # save it
   
-   # create the app user
+   # 01 create / modify the app user
    sql_script="$pgsql_scripts_dir/01.create-qto-app-user.pgsql"
    PGPASSWORD="${postgres_db_useradmin_pw:-}" psql -q -t -X -w -U "${postgres_db_useradmin:-}" \
       -h $postgres_db_host -p $postgres_db_port \
@@ -33,25 +36,18 @@ doRunPgsqlScripts(){
       -v postgres_db_name="${postgres_db_name:-}" \
       -f "$sql_script" "${postgres_db_name:-}" > "$tmp_log_file" 2>&1
    ret=$?
-   cat "$tmp_log_file" # show it 
+   cat "$tmp_log_file" ; cat "$tmp_log_file" >> $log_file # show it and save it 
    test $ret -ne 0 && sleep 3
    test $ret -ne 0 && doExit 1 "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
    test $ret -ne 0 && break
-	cat "$tmp_log_file" >> $log_file # save it
 
-	test -z "${is_sql_biz_as_usual_run:-}" || sleep 1 ; 
    clearTheScreen	
-   
-   echo -e "should run the following files: \n\n" ; 
-	find "$pgsql_scripts_dir" -type f -name "*.sql"|sort -n
+   echo -e "should run the following files: \n\n" ; find "$pgsql_scripts_dir" -type f -name "*.sql"|sort -n
 
-	# run the sql scripts in alphabetical order
+	# run the sql scripts in alphabetical order - hence the file namging convention
    while read -r sql_script ; do 
 
 		relative_sql_script=$(echo $sql_script|perl -ne "s#$PRODUCT_INSTANCE_DIR##g;print")
-
-		# give the poor dev a time to see what is happening
-		test -z "${is_sql_biz_as_usual_run:-}" || sleep $sleep_interval ; clearTheScreen ; 
 
 		doLog "INFO START ::: running $relative_sql_script"
 		echo -e '\n\n'
@@ -60,20 +56,15 @@ doRunPgsqlScripts(){
          -v postgres_db_name="$postgres_db_name" -f "$sql_script" "$postgres_db_name" > "$tmp_log_file" 2>&1
       ret=$?
 
-		cat "$tmp_log_file"
+      cat "$tmp_log_file" ; cat "$tmp_log_file" >> $log_file # show it and save it 
       test $ret -ne 0 && sleep 3
       test $ret -ne 0 && doExit 1 "pid: $$ psql ret $ret - failed to run sql_script: $sql_script !!!"
       test $ret -ne 0 && break
 
-		# and save the tmp log file into the script log file
-		cat "$tmp_log_file" >> $log_file
-		echo -e '\n\n'
+		echo -e '\n\n'; clearTheScreen	
 
 		doLog "INFO STOP  ::: running $relative_sql_script"
 	done < <(find "$pgsql_scripts_dir" -type f -name "*.sql"|sort -n)
 	
 	clearTheScreen ; 
 }
-
-
-# eof file: src/bash/qto/funcs/run-pgsql-scripts.func.sh
