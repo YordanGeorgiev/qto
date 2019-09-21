8cfe40f1e26e1e06782956d750c2f9e4cf286b4d
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-09-21 16:24:37 +0000

    qto-190710174223 fix the "cannot work on more than 1 db within the same session" bug
    Former-commit-id: 3c36ea23a7b28d38b7c13f5f28bf10641df6034a

--- doc/cheats/qto-cheat-sheet.sh
+++ doc/cheats/qto-cheat-sheet.sh
@@ -1,10 +1,19 @@
 # file: doc/cheats/qto-cheat-sheet.sh
 
+# caveat-01: do not use ? char in passwords - it breaks the ui of the doc pages
+# caveat-02: escape the ! char in passwords - does not work in the shell
+# caveats: do not use ? char in passwords ( breaks the ui ), escape the ! char 
+sudo su - postgres -c \
+   "psql --port 15432 -d postgres -c \"ALTER ROLE usrqtoadmin WITH PASSWORD 'foo-bar\!' LOGIN \""
+sudo su - postgres -c \
+   "psql --port 15432 -d postgres -c \"ALTER ROLE usrqtoapp WITH PASSWORD 'foo-bar\!' LOGIN \""
+
 # how-to export the documentation of the desired db. 
 clear ; bash src/bash/qto/qto.sh -a generate-md-docs
 
 # how-to run sql as the postgres user
 clear ; sudo su - postgres -c "psql -d postgres -c \"drop database dev_qto\""
+clear ; sudo su - postgres -c "psql -d postgres -c \"create database dev_qto\""
 
 # how-to load xls sheets into tables , obs - do_truncate_tables=1 WILL truncate the postgres tables
 export do_truncate_tables=0 ; clear ; perl src/perl/qto/script/qto.pl --do xls-to-db --tables system_guide_doc
@@ -14,18 +23,18 @@ source lib/bash/funcs/export-json-section-vars.sh
 
 # optional stop if the env vars from a different project must be loaded ..
 export PROJ_INSTANCE_DIR=`pwd`
+export PROJ_CONF_FILE=`pwd`/cnf/env/dev.env.json
 
 # load the env vars from a section
 doExportJsonSectionVars $PROIJ_INSTANCE_DIR/cnf/env/prd.env.json '.env.db'
+
+# set the authentication for the psql 
+alias psql="PGPASSWORD=${postgres_db_useradmin_pw:-} psql -v -t -X -w -U ${postgres_db_useradmin:-} --host $postgres_db_host --port $postgres_db_port"
 
 # how to add files to the git 
 git add --all; GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" \
 git commit -m "$git_msg" --author "Yordan Georgiev <yordan.georgiev@gmail.com" # --amend
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" git push
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa.ysg" git pull
-
-# set the authentication for the psql 
-alias psql="PGPASSWORD=${postgres_db_useradmin_pw:-} psql -v -t -X -w -U ${postgres_db_useradmin:-} --host $postgres_db_host --port $postgres_db_port"
-
 
 # eof file: doc/cheats/qto-cheat-sheet.sh
