28bcd81859da2e1cf0372ef6464d3d322516815f
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-11-10 21:13:21 +0000

    qto-191110230101 hot-fix for left menu ports

--- src/bash/qto/funcs/provision-https.func.sh
+++ src/bash/qto/funcs/provision-https.func.sh
@@ -1,25 +1,25 @@
 doProvisionHttps(){
 
-   doExportJsonSectionVars $PRODUCT_INSTANCE_DIR/cnf/env/$ENV_TYPE.env.json '.env.app'
-   echo 'at this moment you should have your dns registered according to the docs  if not abort'
-   sleep 6
-   sudo apt-get update
-   sudo apt-get install software-properties-common
-   sudo add-apt-repository universe
-   expect <<- EOF_EXPECT
-      set timeout -1
-      sudo add-apt-repository ppa:certbot/certbot
-      expect "Press [ENTER] to continue or Ctrl-c to cancel adding it."
-      send -- "\r"
-      expect eof
-EOF_EXPECT
-   sudo apt-get install -y certbot python-certbot-nginx
-
-   #src: https://medium.com/@mightywomble/how-to-set-up-nginx-reverse-proxy-with-lets-encrypt-8ef3fd6b79e5
-   echo put in the backgroup and run the following command 
-   echo sudo certbot --nginx -d $web_host #todo:ysg fix hardcoded  - add -d tst.qto -d prd.qto -d qto
-   # todo: add expect automation
-   sleep 1000
+#   doExportJsonSectionVars $PRODUCT_INSTANCE_DIR/cnf/env/$ENV_TYPE.env.json '.env.app'
+#   echo 'at this moment you should have your dns registered according to the docs  if not abort'
+#   sleep 6
+#   sudo apt-get update
+#   sudo apt-get install software-properties-common
+#   sudo add-apt-repository universe
+#   expect <<- EOF_EXPECT
+#      set timeout -1
+#      sudo add-apt-repository ppa:certbot/certbot
+#      expect "Press [ENTER] to continue or Ctrl-c to cancel adding it."
+#      send -- "\r"
+#      expect eof
+#EOF_EXPECT
+#   sudo apt-get install -y certbot python-certbot-nginx
+#
+#   #src: https://medium.com/@mightywomble/how-to-set-up-nginx-reverse-proxy-with-lets-encrypt-8ef3fd6b79e5
+#   echo put in the backgroup and run the following command 
+#   echo sudo certbot --nginx -d $web_host #todo:ysg fix hardcoded  - add -d tst.qto -d prd.qto -d qto
+#   # todo: add expect automation
+#   sleep 1000
    
    source $PRODUCT_INSTANCE_DIR/lib/bash/funcs/export-json-section-vars.sh
    for env in `echo dev tst prd`; do \
