9c36c45133651540c7e209e0439ab9eaa5e27957
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-02-24 13:23:24 +0200

    qto-200224125752 add the items_roles_permissions table

--- src/sql/pgsql/qto/tables/06.create-table-decadally_issues_2020.sql
+++ src/sql/pgsql/qto/tables/06.create-table-decadally_issues_2020.sql
@@ -1,8 +1,8 @@
-  ---- DROP TABLE IF EXISTS decadally_issues ; 
+  ---- DROP TABLE IF EXISTS decadally_issues_2020 ; 
 
-SELECT 'create the "decadally_issues" table'
+SELECT 'create the "decadally_issues_2020" table'
 ; 
-   CREATE TABLE decadally_issues (
+   CREATE TABLE decadally_issues_2020 (
       guid           UUID NOT NULL DEFAULT gen_random_uuid()
     , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
     , category       varchar (30) NOT NULL DEFAULT 'category'
@@ -13,12 +13,12 @@ SELECT 'create the "decadally_issues" table'
     , name           varchar (100) NOT NULL DEFAULT 'name'
     , description    varchar (1000)
     , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
-    , CONSTRAINT pk_decadally_issues_guid PRIMARY KEY (guid)
+    , CONSTRAINT pk_decadally_issues_2020_guid PRIMARY KEY (guid)
     ) WITH (
       OIDS=FALSE
     );
 
-create unique index idx_uniq_decadally_issues_id on decadally_issues (id);
+create unique index idx_uniq_decadally_issues_2020_id on decadally_issues_2020 (id);
 
 
 
@@ -27,17 +27,17 @@ SELECT 'show the columns of the just created table'
 
    SELECT attrelid::regclass, attnum, attname
    FROM   pg_attribute
-   WHERE  attrelid = 'public.decadally_issues'::regclass
+   WHERE  attrelid = 'public.decadally_issues_2020'::regclass
    AND    attnum > 0
    AND    NOT attisdropped
    ORDER  BY attnum
    ; 
 
 --The trigger:
-CREATE TRIGGER trg_set_update_time_on_decadally_issues BEFORE UPDATE ON decadally_issues FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+CREATE TRIGGER trg_set_update_time_on_decadally_issues_2020 BEFORE UPDATE ON decadally_issues_2020 FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
 
 select tgname
 from pg_trigger
 where not tgisinternal
-and tgrelid = 'decadally_issues'::regclass;
+and tgrelid = 'decadally_issues_2020'::regclass;
 
---
+++ src/sql/pgsql/qto/tables/37.create-table-items_roles_permissions.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS items_roles_permissions ; 
+
+SELECT 'create the "items_roles_permissions" table'
+; 
+   CREATE TABLE items_roles_permissions (
+      guid           UUID NOT NULL DEFAULT gen_random_uuid()
+    , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
+    , roles_guid     UUID NOT NULL DEFAULT gen_random_uuid()
+    , items_guid     UUID NOT NULL DEFAULT gen_random_uuid()
+    , read           bool default true
+    , write          bool default false
+    , exec           bool default true
+    , name           varchar (100) NOT NULL DEFAULT 'name...'
+    , description    varchar (200) NULL DEFAULT 'description...'
+    , update_time    timestamp DEFAULT DATE_TRUNC('second', NOW())
+    , CONSTRAINT pk_items_roles_permissions_guid PRIMARY KEY (guid)
+    ) WITH (
+      OIDS=FALSE
+    );
+
+create unique index idx_items_roles_permissions_uniq_id on items_roles_permissions (id);
+
+
+SELECT 'show the columns of the just created table'
+; 
+
+   SELECT attrelid::regclass, attnum, attname
+   FROM   pg_attribute
+   WHERE  attrelid = 'public.items_roles_permissions'::regclass
+   AND    attnum > 0
+   AND    NOT attisdropped
+   ORDER  BY attnum
+   ; 
+
+CREATE TRIGGER trg_set_update_time_on_items_roles_permissions BEFORE UPDATE ON items_roles_permissions 
+   FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+
+select tgname
+from pg_trigger
+where not tgisinternal
+and tgrelid = 'items_roles_permissions'::regclass;
+
