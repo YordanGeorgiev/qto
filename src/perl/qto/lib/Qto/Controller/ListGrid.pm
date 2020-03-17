cc7b25ad6a0dd7acef2f156947b6c9233404749d
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-03-17 12:30:48 +0200

    qto-191125145243 the add new item in view doc does not update properly the ui

--- src/perl/qto/lib/Qto/Controller/ListGrid.pm
+++ src/perl/qto/lib/Qto/Controller/ListGrid.pm
@@ -39,6 +39,7 @@ sub doBuildListControl {
    @hides      = split ( ',' , $to_hides ) if defined ( $to_hides ) ; 
    push ( @hides , 'level' , 'seq' , 'lft','rgt' ) if $table =~ m/^.*_doc/g ; 
 
+   # the default behaviour is to show all the cols, but those wich must be hidden by default
    unless ( defined ( $to_picks )) {
 		foreach my $col ( @$cols ) {
          $control = $control . ",'" . $col . "'" unless ( grep (/^$col$/,@hides )) ; 
@@ -48,16 +49,12 @@ sub doBuildListControl {
       } 
 	} 
 	else {
+      # however if the user explicitly picks cols show ONLY those which are picked, orderly
    	$control = "['id'," ; # it is just the js array definining the cols
    	$control = "[" if $as eq 'print-table' ; 
 		foreach my $to_pick ( @picks ) {
-         unless ( grep (/^$to_pick$/,@hides)) {
-            $control .= "'" . $to_pick . "' , " unless ( $to_pick eq 'id' );
-         }
+         $control .= "'" . $to_pick . "' , " unless ( $to_pick eq 'id' );
 		}
-      foreach my $c ( @hides) {
-         $control .= "'" . $c. "' , " if ( grep (/^$c$/,@picks )) ; 
-      }
 		for (1..3) { chop ( $control ) } ;
    	$control .= ']' ;
 	}
