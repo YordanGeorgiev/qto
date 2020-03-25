42d5b6bb0a37c8f30455291bcdd6cd918f876d81
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-03-25 16:06:22 +0200

    qto-200325161825 add a crontab configuration example
    Former-commit-id: 19fc2b892f9f987ec619a245fbfd627c0464eea0

---
+++ cnf/crontab/crontab-l.sh
@@ -0,0 +1,40 @@
+# file: cnf/crontab/crontab-l.sh
+# usage:
+# to-deploy this configuration
+# crontab cnf/crontab/crontab-l.sh
+
+# every day at 03:00 perform a full db backup in TST
+1 3 * * * cd /home/ubuntu/opt/qto/qto.0.8.1.tst.ysg && bash ./src/bash/qto/qto.sh -a backup-postgres-PRD
+
+# every day at 03:15 perform a full restart of the application layer in TST
+15 3 * * * cd /home/ubuntu/opt/qto/qto.0.8.1.tst.ysg && bash ./src/bash/qto/qto.sh -a mojo-hypnotoad-start
+
+# every day at 03:15 perform a full restart of the application layer in PRD
+15 3 * * * cd /home/ubuntu/opt/qto/qto.0.8.1.prd.ysg && bash ./src/bash/qto/qto.sh -a mojo-hypnotoad-start
+
+# every day at 19:00 perform a full db backup in TST
+1 19 * * * cd /home/ubuntu/opt/qto/qto.0.8.1.tst.ysg && bash ./src/bash/qto/qto.sh -a backup-postgres-db
+
+#
+# 0 3 * * *
+# * * * * * <<command-to-be-executed>>
+# - - - - -
+# | | | | |
+# | | | | +- - - - day of week (0 - 6) (Sunday=0)
+# | | | +- - - - - month (1 - 12)
+# | | +- - - - - - day of month (1 - 31)
+# | +- - - - - - - hour (0 - 23)
+# +--------------- minute
+
+# 
+# edit the crontab
+# export EDITOR=vim; sudo crontab -e
+# 
+# to edit the crontab in batch mode
+# crontab -l | sed 's/>/>>/' | crontab -
+# 
+# to view the crontab
+# sudo crontab -l
+# 
+# when in doubt chk also: https://crontab.guru/#0_19_*_*_*
+# eof file: cnf/crontab/crontab-l.sh
