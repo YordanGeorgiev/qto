9aeefb14237e69ba4d27ec948fd3dc60c8e95f7c
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-05-09 03:16:39 +0300

    qto-200505115752 add the incidents table and record the incidents as separate entities
    Former-commit-id: 3ac061534c210e2cc924c5847be1de6ff7a19a37

---
+++ src/sql/pgsql/qto/tables/18.create-table-incidents.sql
@@ -0,0 +1,42 @@
+-- DROP TABLE IF EXISTS incidents ; 
+
+SELECT 'create the "incidents" table'
+; 
+-- DROP TABLE incidents ; 
+
+CREATE TABLE incidents (
+      guid           UUID NOT NULL DEFAULT gen_random_uuid()
+    , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
+    , prio           integer NOT NULL DEFAULT 1
+    , incidents_status_guid uuid DEFAULT 'cb989a14-d0b8-46e4-b2cc-5e2a974b5d29'::uuid NOT NULL
+    , category       varchar (20) NOT NULL DEFAULT 'category ...'
+    , name           varchar (100) NOT NULL DEFAULT 'name ...'
+    , description    varchar (4000) NOT NULL DEFAULT 'desc ...'
+    , solution_proposal varchar (4000) NULL
+    , owner          varchar (50) NOT NULL DEFAULT 'none'
+    , update_time    timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
+    , CONSTRAINT pk_incidents_guid PRIMARY KEY (guid)
+    ) WITH (
+      OIDS=FALSE
+    );
+
+
+SELECT 'show the columns of the just created table'
+; 
+
+   SELECT attrelid::regclass, attnum, attname
+   FROM   pg_attribute
+   WHERE  attrelid = 'public.incidents'::regclass
+   AND    attnum > 0
+   AND    NOT attisdropped
+   ORDER  BY attnum
+   ; 
+
+
+--The trigger:
+CREATE TRIGGER trg_set_update_time_on_incidents BEFORE UPDATE ON incidents FOR EACH ROW EXECUTE PROCEDURE fnc_set_update_time();
+
+select tgname
+from pg_trigger
+where not tgisinternal
+and tgrelid = 'incidents'::regclass;
