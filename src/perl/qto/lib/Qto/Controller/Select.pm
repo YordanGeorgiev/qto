9b4043d17e38c8c4c318dc68d453489aa3f255d0
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-05-27 21:32:51 +0300

    qto-200526092520 add a demo page for the vue-ctk-date-time-picker control
    Former-commit-id: 6aa909714fa5b87e9ace58eab58af3cedf28219c

--- src/perl/qto/lib/Qto/Controller/Select.pm
+++ src/perl/qto/lib/Qto/Controller/Select.pm
@@ -269,4 +269,13 @@ sub doSelectItemMetaFor {
 
 __END__
 
-# feature-guid: ecd424d7-e5bd-45f1-90c8-10fae1316bf9
+=for comment
+Common error HTTP status codes :
+	400 Bad Request – This means that client-side input fails validation.
+	401 Unauthorized – This means the user isn’t not authorized to access a resource. It usually returns when the user isn’t authenticated.
+	403 Forbidden – This means the user is authenticated, but it’s not allowed to access a resource.
+	404 Not Found – This indicates that a resource is not found.
+	500 Internal server error – This is a generic server error. It probably shouldn’t be thrown explicitly.
+	502 Bad Gateway – This indicates an invalid response from an upstream server.
+	503 Service Unavailable – This indicates that something unexpected happened on server side (It can be anything like server overload, some parts of the system failed, etc.).
+=cut
