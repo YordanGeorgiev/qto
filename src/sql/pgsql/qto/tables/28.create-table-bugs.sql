633ff7f8e2b32c848361867a15a1858b88fcbc30
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-05-30 22:29:38 +0300

    qto-200530222125 add conditional rendering for the calendar control
    Former-commit-id: bb61d2c1f24efb72532a3f81f88aa5ceda810ef3

--- src/sql/pgsql/qto/tables/28.create-table-bugs.sql
+++ src/sql/pgsql/qto/tables/28.create-table-bugs.sql
@@ -1,18 +1,18 @@
 SELECT 'create the "bugs" table'
 ; 
 
-CREATE TABLE bugs (
-      guid           UUID NOT NULL DEFAULT gen_random_uuid()
-    , id             bigint UNIQUE NOT NULL DEFAULT cast (to_char(current_timestamp, 'YYMMDDHH12MISS') as bigint) 
-    , prio           integer NOT NULL DEFAULT 1
-    , issues_status_guid uuid DEFAULT 'cb989a14-d0b8-46e4-b2cc-5e2a974b5d29'::uuid NOT NULL
-    , category_guid uuid DEFAULT '70724562-d83c-446e-94cf-58ced84f3a0e'::uuid NOT NULL
-    , name           varchar (100) NOT NULL DEFAULT 'name ...'
-    , location       character varying(100)
-    , description    varchar (4000) NOT NULL DEFAULT 'desc ...'
-    , solution_proposal varchar (4000) NULL
-    , owner          varchar (50) NOT NULL DEFAULT 'none'
-    , update_time    timestamp NOT NULL DEFAULT DATE_TRUNC('second', NOW())
+CREATE TABLE public.bugs (
+    guid uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    id bigint DEFAULT (to_char(CURRENT_TIMESTAMP, 'YYMMDDHH12MISS'::text))::bigint NOT NULL,
+    prio integer DEFAULT 1 NOT NULL,
+    issues_status_guid uuid DEFAULT 'cb989a14-d0b8-46e4-b2cc-5e2a974b5d29'::uuid NOT NULL,
+    name character varying(100) DEFAULT 'name ...'::character varying NOT NULL,
+    description character varying(4000) DEFAULT 'desc ...'::character varying NOT NULL,
+    solution_proposal character varying(4000),
+    location character varying(100),
+    app_users_guid uuid DEFAULT '2660a6e9-9e6b-4faa-8264-27a92872657b'::uuid NOT NULL,
+    category_guid uuid DEFAULT '70724562-d83c-446e-94cf-58ced84f3a0e'::uuid NOT NULL,
+    update_time timestamp without time zone DEFAULT date_trunc('second'::text, now()) NOT NULL
     , CONSTRAINT pk_bugs_guid PRIMARY KEY (guid)
     ) WITH (
       OIDS=FALSE
