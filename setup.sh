be9fb979da3b0639b34a796d34dae75cc3e983c6
DiVerdi <karambour@mailinator.com>
2020-06-18 16:36:02 +0300

    qto-200618114556 changing setup.sh to pass parameters to child processes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# starts deployer from a folder, then QtoDir redirects to a newly created product_instance_dir
 
 main(){
-	. ./qto/src/bash/deployer/run.sh ; QtoDir
+	. ./qto/src/bash/deployer/run.sh "$@"; QtoDir
 }
 
 main "$@"
