5d42c0f677ad50a2711b39fc8065fc7a39366ad1
Yordan Georgiev <yordan.georgiev@gmail.com>
2020-06-21 01:11:53 +0300

    qto-200621010737 add the generic item status table

--- src/sql/pgsql/qto/tables/2020/2020-06/create-table-monthly_issues_202006.sql
+++ src/sql/pgsql/qto/tables/2020/2020-06/create-table-monthly_issues_202006.sql
@@ -6,13 +6,13 @@ SELECT 'create the "monthly_issues_202006" table'
 CREATE TABLE public.monthly_issues_202006 (
     guid uuid DEFAULT public.gen_random_uuid() NOT NULL,
     id bigint DEFAULT (to_char(CURRENT_TIMESTAMP, 'YYMMDDHH12MISS'::text))::bigint NOT NULL,
+    deadline timestamp without time zone DEFAULT date_trunc('second'::text, now()),
     category_guid uuid DEFAULT '70724562-d83c-446e-94cf-58ced84f3a0e'::uuid NOT NULL,
-    issues_status_guid uuid DEFAULT 'cb989a14-d0b8-46e4-b2cc-5e2a974b5d29'::uuid NOT NULL,
+    status_guid uuid DEFAULT 'cb989a14-d0b8-46e4-b2cc-5e2a974b5d29'::uuid NOT NULL,
     prio integer DEFAULT 0 NOT NULL,
     name character varying(100) DEFAULT 'name...'::character varying NOT NULL,
     description character varying(4000),
     app_users_guid uuid DEFAULT public.gen_random_uuid(),
-    type character varying(30) DEFAULT 'task'::character varying NOT NULL,
     update_time timestamp without time zone DEFAULT date_trunc('second'::text, now())
 );
 
